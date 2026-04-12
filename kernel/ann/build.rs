8dd61a24bad1f37a17e91ae071bd5c6f2a265426
2233admin <1497479966@qq.com>
2026-04-12 20:44:29 +0800

    fix: build.rs proto path uses CARGO_MANIFEST_DIR (was hardcoded Unix /proto/)

--- kernel/ann/build.rs
+++ kernel/ann/build.rs
@@ -1,4 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("/proto/voile.proto")?;
+    let proto_file = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("proto/voile.proto");
+    tonic_build::compile_protos(proto_file)?;
     Ok(())
 }
